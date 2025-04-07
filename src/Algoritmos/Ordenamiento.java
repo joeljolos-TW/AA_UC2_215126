0e12fd2dcb6d731c6e90bb830ca0fb882f7fac0c
joeljolos-TW <joel.cabe2403@gmail.com>
2025-04-07 10:24:59 -0600

    QuickSort Agregado y probado

--- src/Algoritmos/Ordenamiento.java
+++ src/Algoritmos/Ordenamiento.java
@@ -5,19 +5,22 @@
 package Algoritmos;
 
 /**
- * Clase que almacenara los metodos de ordenamiento.
- * Class that will store the order methods.
+ * Clase que almacenara los metodos de ordenamiento. Class that will store the
+ * order methods.
+ *
  * @author joel_
  */
 public class Ordenamiento {
+
     /**
      * Metodo de ordenamiento de burbuja
-     * @param arreglo 
+     *
+     * @param arreglo
      */
     public static void bubbleSort(int[] arreglo) {
-        
+
         for (int i = 1; i < arreglo.length; i++) {//declaracion : 1, comparacion : n-1, incremento: n || = 2n
-            
+
             for (int j = 0; j < arreglo.length - i; j++) {// declaracion : n, comparacion : n^2-n, incremento : n^2 || = 2n^2
                 if (arreglo[j] > arreglo[j + 1]) {// comparacion : n^2 suma : n^2|| = 2n^2
                     int aux = arreglo[j];// declaracion : n^2 || = n^2
@@ -28,12 +31,13 @@ public class Ordenamiento {
         }
 
     } // (2n)+(2n^2)+(2n^2)+(n^2)+(2n^2)+(2n^2) = (9n^2+2n) || O(x^2)
-    
+
     /**
      * Metodo de ordenamiento por insercion
-     * @param arreglo 
+     *
+     * @param arreglo
      */
-    public static void insercion(int[] arreglo) {
+    public static void insercionSort(int[] arreglo) {
 
         for (int i = 0; i < arreglo.length; i++) {
 
@@ -46,23 +50,71 @@ public class Ordenamiento {
             arreglo[j + 1] = key;
         }
     }
-    
+
     /**
      * Metodo de ordenamiento por busqueda
-     * @param arreglo 
+     *
+     * @param arreglo
      */
-    public static void seleccion(int[] arreglo){
-        for (int i = 0; i < arreglo.length-1; i++) {
+    public static void seleccionSort(int[] arreglo) {
+        for (int i = 0; i < arreglo.length - 1; i++) {
             int indMenor = i;
-            for (int j = i+1; j < arreglo.length; j++) {
-                if (arreglo[j]<arreglo[indMenor]){
+            for (int j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[j] < arreglo[indMenor]) {
                     indMenor = j;
                 }
             }
             int aux = arreglo[i];
-            arreglo[i]=arreglo[indMenor];
-            arreglo[indMenor]=aux;
+            arreglo[i] = arreglo[indMenor];
+            arreglo[indMenor] = aux;
         }
 
+    }
+    /**
+     * metodo recursivo de ordenamiento Rapido
+     * @param nums
+     * @param low
+     * @param high 
+     */
+    public static void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+
+            int pivot = partition(nums, low, high);
+
+            quickSort(nums, low, pivot - 1);
+
+            quickSort(nums, pivot + 1, high);
+        }
+    }
+    /**
+     * metodo para encontrar el pivote del arreglo
+     * @param nums
+     * @param low
+     * @param high
+     * @return 
+     */
+    public static int partition(int[] nums, int low, int high) {
+
+        int pivot = nums[high];
+
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (nums[j] <= pivot) {
+
+                i++;
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        int temp = nums[i + 1];
+        nums[i + 1] = nums[high];
+        nums[high] = temp;
+
+        return i + 1;
     }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,28 +7,34 @@ import Algoritmos.Busquedas;
 import Algoritmos.Ordenamiento;
 import java.util.Arrays;
 /**
- * Clase principal para las pruebas de los algoritmos.
- * Principal class to make tests of the algorithms.
+ *
  * @author joel_
  */
 public class Main {
 
-    
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         int[] arr = {4,10,9,8};
         System.out.println("El arreglo al inicio: "+Arrays.toString(arr));
         Ordenamiento.bubbleSort(arr);
-        System.out.println("El arreglo al final: "+Arrays.toString(arr));
+        System.out.println("El arreglo al final: "+Arrays.toString(arr)+"\n");
         
         int[] arr1= {5,21,48,10,1};
         System.out.println("El arreglo al inicio: "+Arrays.toString(arr1));
-        Ordenamiento.insercion(arr1);
-        System.out.println("El arreglo al Final: "+Arrays.toString(arr1));
+        Ordenamiento.insercionSort(arr1);
+        System.out.println("El arreglo al Final: "+Arrays.toString(arr1)+"\n");
         
         int[] arr2= {37,7,51,22,9};
         System.out.println("El arreglo al inicio: "+Arrays.toString(arr2));
-        Ordenamiento.seleccion(arr2);
-        System.out.println("El arreglo al final: "+Arrays.toString(arr2));
+        Ordenamiento.seleccionSort(arr2);
+        System.out.println("El arreglo al final: "+Arrays.toString(arr2)+"\n");
+        
+        int[] arr3 = {42, 17, 89, 5, 63, 28, 91, 34, 76, 12};
+        System.out.println("El arreglo al inicio: "+Arrays.toString(arr3));
+        Ordenamiento.quickSort(arr3,0,arr3.length-1);
+        System.out.println("El arreglo al final: "+Arrays.toString(arr3));
     }
     
 }
